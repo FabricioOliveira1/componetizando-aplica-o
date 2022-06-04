69c4fe2500ed2b651e6857bfca0996dae190c9c2
Fabricio Oliveira <fabricioliveira021@gmail.com>
2022-06-04 17:26:15 -0300

    Desafio Concluido

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,34 @@
-export function SideBar() {
-  // Complete aqui
+import { Button } from './Button';
+
+interface Props {
+  selectedGenreId: number
+  handleClickButton: (id:number) => void; //typing a function with params
+  genres: {
+    id: number;
+    name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+    title: string;
+    }[] // This interface means that "genres", is an array of objects with id, name, and title properties.
+}
+
+export function SideBar (props: Props) {
+
+  const genres = props.genres; 
+  
+  return (
+     <nav className="sidebar">
+            <span>Watch<p>Me</p></span>
+          
+          <div className="buttons-container">
+            {genres.map(genre => (
+              <Button
+                key={String(genre.id)}
+                title={genre.title}
+               iconName={genre.name}
+                onClick={() => props.handleClickButton(genre.id)}
+                selected={props.selectedGenreId === genre.id}
+              />
+            ))}
+          </div>
+      </nav>
+  )
 }
